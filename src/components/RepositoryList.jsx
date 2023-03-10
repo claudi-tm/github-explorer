e2c8a8d253b30bddd1496c54ede9bc61aa244129
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-10 16:42:13 +0200

    action: created useEffect to add repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,31 @@
 import { RepositoryItem } from "./RepositoryItem";
-import "../styles/repositories.scss"
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+	const [repositories, setReposiotries] = useState([]);
+
+	useEffect(() => {
+		fetch("https://api.github.com/orgs/rocketseat/repos")
+			.then((response) => {
+				response.json();
+			})
+			.then((data) => {
+				setReposiotries(data);
+			});
+	}, []);
+
 	return (
 		<section className="repository-list">
 			<h1>Lista de repositórios</h1>
 
 			<ul>
-				<RepositoryItem title="unform" content="Forms in React Js"></RepositoryItem>
-				
+				<RepositoryItem
+					title="unform"
+					content="Forms in React Js"
+				></RepositoryItem>
 			</ul>
 		</section>
 	);
