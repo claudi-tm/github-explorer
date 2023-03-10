30f9f15208366847e6f7db55b8c8f205fe03e3dc
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-10 13:28:56 +0200

    configurated of web packet server

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 const { dirname } = require("path");
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+	mode: "development",
 	// diz qual é a pagina principal da noss aplicação
 	// entry: "src/index.jsx",
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html")
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    }
+	entry: path.resolve(__dirname, "src", "index.jsx"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".jsx", ".js"],
+	},
+	devServer: {
+		static: path.resolve(__dirname, "public"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+		],
+	},
 };
