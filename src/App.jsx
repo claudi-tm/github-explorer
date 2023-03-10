7349d20a9c2ce49104cb976d2bc89d5c52bd621f
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-10 15:32:56 +0200

    add: react refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { RepositoryList } from './components/RepositoryList'
-import './styles/global.scss'
+import { Counter } from "./components/Counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
 export function App() {
-   
-    return <RepositoryList></RepositoryList>
+	return (
+		<>
+			<RepositoryList></RepositoryList>
+			<Counter></Counter>
+		</>
+	);
 }
