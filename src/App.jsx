51f7560ad612c765cde8fab44970d4e5700af918
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-10 16:22:29 +0200

    added style to repositories

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Counter } from "./components/Counter";
+
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
@@ -6,7 +6,6 @@ export function App() {
 	return (
 		<>
 			<RepositoryList></RepositoryList>
-			<Counter></Counter>
 		</>
 	);
 }
