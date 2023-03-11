c179292c1402a06c059c6f3c61270f94bde1aa00
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-11 19:43:03 +0200

    ended tsc config

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,13 @@ module.exports = {
 	mode: isDevelopment ? "development" : "production",
 	// diz qual é a pagina principal da noss aplicação
 	// entry: "src/index.jsx",
-	entry: path.resolve(__dirname, "src", "index.jsx"),
+	entry: path.resolve(__dirname, "src", "index.tsx"),
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
 	},
 	resolve: {
-		extensions: [".jsx", ".js"],
+		extensions: [".jsx", ".js", "ts", "tsx"],
 	},
 
 	devtool: isDevelopment ? "eval-source-map" : "source-map",
@@ -33,7 +33,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.jsx$/,
+				test: /\.(j|t)sx$/,
 				exclude: /node_modules/,
 				use: {
 					loader: "babel-loader",
